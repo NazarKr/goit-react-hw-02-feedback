d691d02273d61158f29412acec897a30502fad34
NazarKr <nazarok1990@gmail.com>
2023-05-04 13:50:59 +0300

    add ts

--- src/components/feedback/Feedback.tsx
+++ src/components/feedback/Feedback.tsx
@@ -2,35 +2,41 @@ import React, { Component } from 'react';
 import { FeedbackDiv } from './feedback.styled';
 import FeedbackOptions from './ButtonsComponents';
 import Statistics from './Statistics';
-import Section from './Section'
-import Notification from './Notification'
+import Section from './Section';
+import Notification from './Notification';
 
-class Feedback extends Component {
-  state = {
+interface FeedbackState {
+  good: number;
+  neutral: number;
+  bad: number;
+}
+
+class Feedback extends Component<{}, FeedbackState> {
+  state: FeedbackState = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
 
-  optionKey = Object.keys(this.state);
+  optionKey: string[] = Object.keys(this.state);
 
-  onLeaveFeedback = name => {
-    this.setState(prevState => {
-      return { [name]: prevState[name] + 1 }
-    })
-  }
+  onLeaveFeedback = (name: keyof FeedbackState) => {
+    this.setState((prevState: FeedbackState) => ({
+      ...prevState,
+      [name]: prevState[name] + 1,
+    }));
+  };
 
-  totalFeedback = () => {
-    const total = this.state.good + this.state.neutral + this.state.bad;
+  totalFeedback = (): number => {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
     return total;
   };
 
-  positiveFeedback = () => {
-    const positive = Math.round(
-      (this.state.good * 100) /
-      (this.state.good + this.state.neutral + this.state.bad)
-    );
-    return positive || '';
+  positiveFeedback = (): number | null => {
+    const { good, neutral, bad } = this.state;
+    const positive = Math.round((good * 100) / (good + neutral + bad));
+    return isNaN(positive) ? null : positive;
   };
 
   render() {
@@ -55,8 +61,9 @@ class Feedback extends Component {
           </Section>
         )}
 
-        {this.totalFeedback() === 0 &&
-          <Notification message="There is no feedback" />}
+        {this.totalFeedback() === 0 && (
+          <Notification message="There is no feedback" />
+        )}
       </FeedbackDiv>
     );
   }
